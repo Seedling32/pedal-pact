6cb85203118bb0fa7ef70feb35c69e5ed3aeac71
Seedling <david.r.graham32@gmail.com>
2025-01-16 19:00:00 -0500

    More set-up Create (root)folder and new layout.tsx Create constants/index for app name/description and server url. Update app/layout to reflect new constants values.

---
+++ lib/constants/index.ts
@@ -0,0 +1,7 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Pedal Pact';
+
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  'A community-driven app for sharing bike routes, organizing group rides, and connecting cyclists in the Asheville, NC area.';
+
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000';
