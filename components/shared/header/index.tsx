ee64ca3cdc4e4e13fe2059de14d0aed7f0bb104d
Seedling <david.r.graham32@gmail.com>
2025-01-16 20:01:04 -0500

    Getting the colors right!

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,12 +1,12 @@
 import { Button } from '@/components/ui/button';
-import { UserRound } from 'lucide-react';
+import { UserRound, UserRoundPlus } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { APP_NAME } from '@/lib/constants';
 
 const Header = () => {
   return (
-    <header className="w-full border-b">
+    <header className="bg-slate-800 w-full border-b">
       <div className="wrapper flex-between">
         <div className="flex-start">
           <Link href="/" className="flex-start">
@@ -16,14 +16,25 @@ const Header = () => {
               height={48}
               width={48}
               priority={true}
+              className="rounded-lg"
             />
-            <span className="hidden lg:block font-bold text-2xl ml-3">{APP_NAME}</span>
+            <span className="hidden lg:block font-bold text-2xl ml-3 text-white">{APP_NAME}</span>
           </Link>
         </div>
-        <div className="space-x-2">
-          <Button asChild>
+        <div className="flex space-x-2">
+          <div className="flex items-center space-x-3 mr-2 text-white">
+            <Link href="/rides">Rides</Link>
+            <Link href="/about-us">About Us</Link>
+            <Link href="/contact">Contact</Link>
+          </div>
+          <Button asChild variant="ghost">
             <Link href="/sign-in">
               <UserRound /> Sign In
+            </Link>
+          </Button>
+          <Button asChild>
+            <Link href="/register">
+              <UserRoundPlus /> Register
             </Link>
           </Button>
         </div>
