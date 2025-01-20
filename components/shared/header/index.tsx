af8f2d4bb157ff4cbfbcfeddcbc6a754d98f7f86
Seedling <david.r.graham32@gmail.com>
2025-01-20 16:38:30 -0500

    Install sheet menu install sheet from shadcn ui Move "nav" from header/index to menu.tsx and create actual nav with menu. Style the two navs for different sized screens using the sheet menu for smaller screens.

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,9 +1,7 @@
-import { Button } from '@/components/ui/button';
-import { UserRound, UserRoundPlus } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { APP_NAME } from '@/lib/constants';
-import ModeToggle from './mode-toggle';
+import Menu from './menu';
 
 const Header = () => {
   return (
@@ -22,24 +20,7 @@ const Header = () => {
             <span className="hidden lg:block font-bold text-2xl ml-3 text-white">{APP_NAME}</span>
           </Link>
         </div>
-        <div className="flex space-x-2">
-          <div className="flex items-center space-x-3 mr-2 text-white">
-            <ModeToggle />
-            <Link href="/rides">Rides</Link>
-            <Link href="/about-us">About Us</Link>
-            <Link href="/contact">Contact</Link>
-          </div>
-          <Button asChild variant="ghost">
-            <Link href="/sign-in">
-              <UserRound /> Sign In
-            </Link>
-          </Button>
-          <Button asChild>
-            <Link href="/register">
-              <UserRoundPlus /> Register
-            </Link>
-          </Button>
-        </div>
+        <Menu />
       </div>
     </header>
   );
---
+++ components/shared/header/menu.tsx
@@ -0,0 +1,73 @@
+import Link from 'next/link';
+import ModeToggle from './mode-toggle';
+import { Button } from '@/components/ui/button';
+import { EllipsisVertical, UserRound, UserRoundPlus } from 'lucide-react';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet';
+
+const Menu = () => {
+  return (
+    <div className="flex justify-end gap-3">
+      <nav className="hidden md:flex items-center w-full max-w-s gap-2 text-white">
+        <Link href="/rides" className="hover:border-b">
+          Rides
+        </Link>
+        <Link href="/about-us" className="hover:border-b">
+          About Us
+        </Link>
+        <Link href="/contact" className="hover:border-b">
+          Contact
+        </Link>
+        <Button asChild variant="ghost" className="ml-2">
+          <Link href="/sign-in">
+            <UserRound /> Sign In
+          </Link>
+        </Button>
+        <Button asChild>
+          <Link href="/register">
+            <UserRoundPlus /> Register
+          </Link>
+        </Button>
+        <ModeToggle />
+      </nav>
+      <nav className="md:hidden">
+        <Sheet>
+          <SheetTrigger className="align-middle">
+            <EllipsisVertical />
+          </SheetTrigger>
+          <SheetContent className="flex flex-col items-start">
+            <SheetTitle>Menu</SheetTitle>
+            <Link href="/rides" className="hover:underline">
+              Rides
+            </Link>
+            <Link href="/about-us" className="hover:underline">
+              About Us
+            </Link>
+            <Link href="/contact" className="hover:underline">
+              Contact
+            </Link>
+            <Button asChild variant="ghost">
+              <Link href="/sign-in">
+                <UserRound /> Sign In
+              </Link>
+            </Button>
+            <Button asChild>
+              <Link href="/register">
+                <UserRoundPlus /> Register
+              </Link>
+            </Button>
+            <ModeToggle />
+            <SheetDescription></SheetDescription>
+          </SheetContent>
+        </Sheet>
+      </nav>
+    </div>
+  );
+};
+
+export default Menu;
