de653fcab6628dd30a3d8ace0edfa638b0da2252
Seedling <david.r.graham32@gmail.com>
2025-02-08 10:44:05 -0500

    Rewrite function comments and make space.

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -13,18 +13,13 @@ const pool = new Pool({ connectionString });
 // Instantiates the Prisma adapter using the Neon connection pool to handle the connection between Prisma and Neon.
 const adapter = new PrismaNeon(pool);
 
-// Extends the PrismaClient with a custom result transformer to convert the price and rating fields to strings.
+// Extends the PrismaClient with a custom result transformer to eventually convert the distance from  and rating fields to strings.
 export const prisma = new PrismaClient({ adapter }).$extends({
   result: {
     ride: {
       shortDescription: {
         compute(ride) {
           return ride.shortDescription.toString();
-        },
-      },
-      longDescription: {
-        compute(ride) {
-          return ride.longDescription.toString();
         },
       },
     },
--- src/lib/actions/ride.actions.ts
+++ src/lib/actions/ride.actions.ts
@@ -1,9 +1,9 @@
 'use server';
+
 import { prisma } from '@/src/db/prisma';
 import { convertToPlainObject } from '../utils';
 
-// Get rides
-
+// Get rides to display on the ride page.
 export async function getLatestRides() {
   const data = await prisma.ride.findMany({
     take: 8,
@@ -15,6 +15,7 @@ export async function getLatestRides() {
   return convertToPlainObject(data);
 }
 
+// Get individual ride details based on the slug.
 export async function getRideBySlug(slug: string) {
   return await prisma.ride.findFirst({
     where: { slug: slug },
