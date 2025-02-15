7798fb765a582241d54ed7b668666c6e5e021826
Seedling <david.r.graham32@gmail.com>
2025-02-15 16:23:04 -0500

    Create callback URL functionality

--- src/app/(auth)/sign-in/credentials-signin-form.tsx
+++ src/app/(auth)/sign-in/credentials-signin-form.tsx
@@ -8,12 +8,16 @@ import Link from 'next/link';
 import { useActionState } from 'react';
 import { useFormStatus } from 'react-dom';
 import { signInWithCredentials } from '@/src/lib/actions/user.actions';
+import { useSearchParams } from 'next/navigation';
 
 const CredentialsSignInForm = () => {
   const [data, action] = useActionState(signInWithCredentials, {
     success: false,
     message: '',
   });
+
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams?.get('callbackUrl') || '/';
 
   const SignInButton = () => {
     const { pending } = useFormStatus();
@@ -27,6 +31,7 @@ const CredentialsSignInForm = () => {
 
   return (
     <form action={action}>
+      <input type="hidden" name="callbackurl" value={callbackUrl} />
       <div className="space-y-6">
         <div>
           <Label htmlFor="email">Email</Label>
