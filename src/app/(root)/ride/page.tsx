b750f83d64ff52ec7f08ef1529bb4d686071894a
Seedling <david.r.graham32@gmail.com>
2025-02-15 15:16:55 -0500

    Start building sign in page Create new auth layout Create sign in page Add custom title to ride page.tsx

--- src/app/(root)/ride/page.tsx
+++ src/app/(root)/ride/page.tsx
@@ -1,5 +1,10 @@
 import RideList from '@/src/components/shared/rides/ride-list';
 import { getLatestRides } from '@/src/lib/actions/ride.actions';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Rides',
+};
 
 const RidesPage = async () => {
   const latestRides = await getLatestRides();
